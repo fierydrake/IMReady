f5a2d0dfa9836a79ab40aa1eac9350114135cf81
mjt <mjt@harbinger.(none)>
2011-02-26 21:46:14 +0000

    User story: I'm new
    - Server responds to:
      > 1) GET /participant/<username>
      > 2) POST /participants
             username: <username>
             name: <name>
    - GET request (1) checks for existing user in database
    - POST request (2) checks for existing/creates user in database
    - *Basic* error responses
    - *Basic* logging
    - *Basic* database access (DBI) and credentials
    - Provided example credentials file, edit and rename to "credentials"
    - Provided database schema SQL (MySQL dialect)
      > Can import with "mysql -D dbname < dbschema.mysql.sql"
    
    - Updated .gitignore to ignore credentials file(s)
      > I'm using "credentials.servername" for credentials for non-local servers
      > credentials.example is the exception, and it NOT ignored
      > (Also ignoring file "deploy.sh", for my convenience - shoot me)

--- IMReadyServer.pl
+++ IMReadyServer.pl
@@ -6,31 +6,65 @@ use HTTP::Status;
 use HTTP::Response;
 use HTTP::Headers;
 use HTTP::Message;
-use JSON;
+use CGI;
+#use JSON;
+
+my ($dbName, $dbUser, $dbPass) = readConfig();
 
 print "IMReady Server starting\n";
+
+my $unamePattern = "[A-Za-z_0-9]+";
 
 my $running = 1;
 my %meetings = ();
 my $latestKey = 1;
-  
+
+# Let's connect to a db
+use DBI;
+my $db = DBI->connect("DBI:mysql:$dbName", $dbUser, $dbPass) || die "No db connect: $DBI::errstr";
+ 
 while($running){
     my $daemon = HTTP::Daemon->new( LocalPort => 54321 ) || die "Oops - Failed to even start a server.";
 
     while (my $connection = $daemon->accept) {
-        while (my $request = $connection->get_request) {
-            if ( $request->uri->path eq '/meetings' ){
+        debug('Accepted connection');
+        if (my $request = $connection->get_request) {
+            if ( $request->uri->path eq '/meetings' ) {
                 if ( $request->method eq 'GET' ){
+                    debug('Return meetings');
                     returnMeetings($connection);
                 } elsif ( $request->method eq 'POST' ) {
+                    debug('Create meeting');
                     createMeeting($connection, $request);
+                } else {
+                    $connection->send_error();
+                }
+            } elsif ( $request->uri->path eq '/participants' ) {
+                if ( $request->method eq 'POST' ) {
+                    debug('Create participant');
+                    createParticipant($connection, $request);
+                } else {
+                    $connection->send_error();
+                }
+            } elsif ( $request->uri->path =~ /^\/participant\/($unamePattern)$/ ) {
+                my $username = $1;
+                if ( $request->method eq 'GET' ) {
+                    debug('Return participant');
+                    returnParticipant($connection, $username);
+                } else {
+                    $connection->send_error();
                 }
             } else {
                 $connection->send_error();
             }
         }
+        $connection->close;
+        undef($connection);
     }
 }
+
+# Not executed atm, can only quit server with signal atm
+$db->disconnect();
 
 sub returnMeetings {
     my $conn = shift;
@@ -54,4 +88,78 @@ sub createMeeting {
 
     my $resp = HTTP::Response->new(RC_OK, "", $hdr, $content);
     $conn->send_response($resp);
+}
+
+sub createParticipant {
+    my $conn = shift;
+    my $req = shift;
+
+    my $hdr     = HTTP::Headers->new(Content_Type => 'application/json',
+                                     Connection   => 'close');
+    my $q = CGI->new($req->content);
+    my $username = $q->param('username');
+    my $name = $q->param('name');
+
+    debug('Look-up user');
+    my $s = $db->prepare("SELECT name,username FROM users WHERE username=?");
+    $s->execute($username);
+    my $result = $s->fetchrow_hashref();
+    if ($result) {
+        debug('User already taken');
+        $conn->send_error();
+    } else {
+        debug('Create user');
+        $s = $db->prepare("INSERT INTO users (name,username) VALUES (?,?)");
+        my $rows = $s->execute($name, $username);
+        if ($rows == 1) {
+            debug("Success");
+            my $content = "{\"username\":\"$username\", \"name\":\"$name\"}";
+
+            my $resp = HTTP::Response->new(RC_OK, "", $hdr, $content);
+            $conn->send_response($resp);
+        } else {
+            debug("Failure, $rows rows inserted");
+            $conn->send_error(500, "Failed to process");
+        }
+    }
+}
+
+sub returnParticipant {
+    my $conn = shift;
+    my $username = shift;
+
+    my $hdr     = HTTP::Headers->new(Content_Type => 'application/json',
+                                     Connection   => 'close');
+    
+    debug('Look-up user');
+    my $s = $db->prepare("SELECT name,username FROM users WHERE username=?");
+    $s->execute($username);
+    my $result = $s->fetchrow_hashref();
+    if ($result) {
+        debug('User found, send result');
+        # TODO Marshall JSON properly
+        my $name = $result->{'name'};
+        my $content = qq({"username":"$username", "name":"$name"});
+
+        my $resp = HTTP::Response->new(RC_OK, "", $hdr, $content);
+        $conn->send_response($resp);
+    } else {
+        debug('User not found');
+        $conn->send_error(404, "Participant not found");
+    }
+}
+
+sub debug {
+    my $msg = shift;
+    print STDERR $msg;
+    print STDERR "\n";
+}
+
+sub readConfig {
+    open(my $f, '<', 'credentials') || die "Failed to get credentials: $!";
+    my $dbnm = readline($f); chomp($dbnm);
+    my $user = readline($f); chomp($user);
+    my $pass = readline($f); chomp($pass);
+    close($f);
+    return ($dbnm, $user, $pass);
 }
