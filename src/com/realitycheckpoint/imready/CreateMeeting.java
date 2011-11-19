da2053bcc84f2d162536f111d548b0354193c235
unknown <Mike@fierydrake.(none)>
2011-11-19 16:19:30 +0000

    Removed unused import

--- src/com/realitycheckpoint/imready/CreateMeeting.java
+++ src/com/realitycheckpoint/imready/CreateMeeting.java
@@ -14,7 +14,6 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import android.app.Activity;
-import android.app.Instrumentation.ActivityResult;
 import android.content.SharedPreferences;
 import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
