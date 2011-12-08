ed3d09ec1d0ab7ecc5a264c9c677148b8beeff4e
andy <andy@kusanagi>
2011-12-08 23:36:28 +0000

    Server now handles setting the notified flag Added X-IMReady-Auth-ID to REST doc

--- IMReadyServer.pl
+++ IMReadyServer.pl
@@ -29,7 +29,7 @@ sub main{
     debug('Starting server on port <' . $listenerPort . '>');
     while($running){
         my $daemon = HTTP::Daemon->new( LocalPort => $listenerPort ) || die "Oops - Failed to even start a server.";
-    
+
         while (my $connection = $daemon->accept) {
             debug('Accepted connection');
             if (my $request = $connection->get_request) {
@@ -39,28 +39,28 @@ sub main{
                         if ( $request->uri->path =~ /^\/user\/($unamePattern)$/ ) {
                             my $username = $1;
                             debug('Return user <' . $username . '>');
-                            returnUser($connection, $username);
+                            returnUser($connection, $request, $username);
                         } else {
                             debug('Invalid user request');
                             $connection->send_error(404, 'Invalid user');
                         }
                     } elsif ( $request->uri->path eq '/meetings' ) {
                         debug('Return meetings');
-                        returnMeetings($connection);
+                        returnMeetings($connection, $request);
                     } elsif ( $request->uri->path =~ /^\/meetings\/($unamePattern)$/ ) {
                         my $username = $1;
                         debug('Return meetings for user <' . $username . '>');
-                        returnUserMeetings($connection, $username);
+                        returnUserMeetings($connection, $request, $username);
                     } elsif ( $request->uri->path =~ /^\/meeting\/(\d+)$/ ) {
                         my $meeting = $1;
                         debug('Return meeting <' . $meeting . '>');
-                        returnMeeting($connection, $meeting);
+                        returnMeeting($connection, $request, $meeting);
                     } else {
                         $connection->send_error(500, 'Internal error');
                     }
                 } elsif ( $request->method eq 'POST' ) {
                     if ( $request->uri->path eq '/users' ) {
-                        debug('Create participant');
+                        debug('Create user');
                         createUser($connection, $request);
                     } elsif ( $request->uri->path eq '/meetings' ) {
                         debug('Create meeting');
@@ -81,16 +81,42 @@ sub main{
                     } else {
                         $connection->send_error(500, 'Internal error');
                     }
+                } elsif ( $request->method eq 'DELETE' ) {
+                    debug('Askes to DELETE but there\'s no D in the CRUD yet');
+                    $connection->send_error(500, 'Internal error');
+                } else {
+                    $connection->send_error(500, 'Internal error');
                 }
             }
+            debug('Done with connection');
             $connection->close;
             undef($connection);
         }
     }
 }
 
+sub returnUser {
+    my $conn = shift;
+    my $req  = shift;
+    my $id   = shift;
+    debug('Look-up user <' . $id . '>');
+
+    if( userExists( $id ) ) {
+        my $content = "{\"username\":\"" . $id . "\", \"name\":\"" . getNickname($id) . "\"}";
+
+        my $hdr     = HTTP::Headers->new( Content_Type => 'application/json',
+                                          Connection   => 'close');
+        my $resp = HTTP::Response->new(RC_OK, "", $hdr, $content);
+        $conn->send_response($resp);
+    } else {
+        debug('User id not found');
+        $conn->send_error(404, "Participant not found");
+    }
+}
+
 sub returnMeetings {
     my $conn = shift;
+    my $req  = shift;
 
     my $content;
 
@@ -123,12 +149,43 @@ sub returnMeetings {
 
 sub returnUserMeetings {
     my $conn = shift;
+    my $req  = shift;
     my $id   = shift;
 
+    my $requestingUserID = $req->header('X-IMReady-Auth-ID');
+    if( $requestingUserID ) {
+        debug('Request came from user <' . $requestingUserID . '>' );
+
+        my $valid = userValid($requestingUserID);
+        if ( $valid == 0 ) {
+            debug('Invalid user id <' . $requestingUserID . '>');
+            $conn->send_error(500, "Internal Error");
+            return;
+        }
+
+        my $exists = userExists($requestingUserID);
+        if ( $exists == 0 ) {
+            debug('User id <' . $requestingUserID . '> not found');
+            $conn->send_error(404, "User id not found");
+            return;
+        } elsif ( $exists == -1 ) {
+            $conn->send_error(500, "Internal error");
+            return;
+        }
+    }
+    
     my $content;
+
+    my $valid = userValid($id);
+    if ( $valid == 0 ) {
+        debug('Invalid user id <' . $id . '>');
+        $conn->send_error(500, "Internal Error");
+        return;
+    }
 
     my $exists = userExists($id);
     if ( $exists == 0 ) {
+        debug('User id <' . $id . '> not found');
         $conn->send_error(404, "User id not found");
         return;
     } elsif ( $exists == -1 ) {
@@ -149,6 +206,10 @@ sub returnUserMeetings {
         $content  = "[";
         foreach my $meeting (sort @meetings){
             $content .= "{\"id\": \"" . $meeting . "\", \"name\": \"" . $redis->get("meeting:$meeting:name") . "\"},";
+            if( $requestingUserID && $requestingUserID eq $id ) {
+                debug('Setting user <' . $id . '> in meeting <' . $meeting . '> notified to 1');
+                $redis->set("meeting:$meeting:$id:notified", 1);
+            }
         }
         chop $content;
         $content .= "]";
@@ -165,7 +226,30 @@ sub returnUserMeetings {
 
 sub returnMeeting {
     my $conn    = shift;
+    my $req     = shift;
     my $meeting = shift;
+
+    my $requestingUserID = $req->header('X-IMReady-Auth-ID');
+    if( $requestingUserID ) {
+        debug('Request came from user <' . $requestingUserID . '>' );
+
+        my $valid = userValid($requestingUserID);
+        if ( $valid == 0 ) {
+            debug('Invalid user id <' . $requestingUserID . '>');
+            $conn->send_error(500, "Internal Error");
+            return;
+        }
+
+        my $exists = userExists($requestingUserID);
+        if ( $exists == 0 ) {
+            debug('User id <' . $requestingUserID . '> not found');
+            $conn->send_error(404, "User id not found");
+            return;
+        } elsif ( $exists == -1 ) {
+            $conn->send_error(500, "Internal error");
+            return;
+        }
+    }
 
     my $redis;
     unless ( $redis = Redis->new( server => 'localhost:6379', encoding => undef ) ) {
@@ -184,6 +268,10 @@ sub returnMeeting {
         if ( scalar @participants > 0 ) {
             $content .= " \"participants\": [";
             foreach my $participant (sort @participants){
+                if( $requestingUserID && $requestingUserID eq $participant ) {
+                    debug('Setting user <' . $participant . '> in meeting <' . $meeting . '> notified to 1');
+                    $redis->set("meeting:$meeting:$participant:notified", 1);
+                }
                 $content .= "{\"id\": \"" . $participant .
                             "\", \"defaultNickname\": \"" . getNickname($participant) .
                             "\", \"state\": \"" . $redis->get("meeting:$meeting:$participant:state") .
@@ -206,88 +294,6 @@ sub returnMeeting {
     $conn->send_response($resp);
 }
 
-sub createMeeting {
-    my $conn  = shift;
-    my $req   = shift;
-
-    my $q = CGI->new($req->content);
-    my $id = $q->param('creator');
-    my $meetingName = $q->param('name');
-    debug('Asked to make meeting for <' . $id . '> called <' . $meetingName . '>');
-
-    my $exists = userExists($id);
-    if ( $exists == 0 ) {
-        $conn->send_error(404, "User id not found");
-        return;
-    } elsif ( $exists == -1 ) {
-        $conn->send_error(500, "Internal Error");
-        return;
-    }
-
-    my $redis;
-    unless ( $redis = Redis->new( server => 'localhost:6379', encoding => undef ) ) {
-        debug('Failed to connect to Redis');
-        $conn->send_error(500, 'Internal error');
-        return;
-    }
-
-    # Create the Redis keys for the meeting
-    my $meeting = $redis->incr('util:counter:meetings');
-    $redis->set("meeting:$meeting:name" => $meetingName);
-    $redis->set("meeting:$meeting:state" => 0);
-    $redis->sadd("meeting:$meeting:participants" => $id);
-    $redis->set("meeting:$meeting:$id:state" => 0);
-    $redis->set("meeting:$meeting:$id:notified" => 1);
-    $redis->sadd("util:list:meetings" => $meeting);
-    $redis->sadd("user:$id:meetings" => $meeting);
-    $redis->quit;
-
-    my $hdr     = HTTP::Headers->new(Content_Type => 'application/json',
-                                     Connection   => 'close');
-    my $content = "{\"id\":\"$meeting\"}";
-
-    my $resp = HTTP::Response->new(RC_OK, "", $hdr, $content);
-    $conn->send_response($resp);
-}
-
-sub addParticipant {
-    my $conn    = shift;
-    my $req     = shift;
-    my $meeting = shift;
-    
-    my $q = CGI->new($req->content);
-    my $id = $q->param('id');
-    debug('Add user <' . $id . "> to meeting <" . $meeting . ">" );
-
-    my $exists = userExists($id);
-    if ( $exists == 0 ) {
-        $conn->send_error(404, "User id not found");
-        return;
-    } elsif ( $exists == -1 ) {
-        $conn->send_error(500, "Internal error");
-        return;
-    }
-
-    my $redis;
-    unless ( $redis = Redis->new( server => 'localhost:6379', encoding => undef ) ) {
-        debug('Failed to connect to Redis');
-        $conn->send_error(500, "Internal error");
-        return;
-    }
-    $redis->sadd("meeting:$meeting:participants" => $id);
-    $redis->set("meeting:$meeting:$id:state" => 0);
-    $redis->set("meeting:$meeting:$id:notified" => 0);
-    $redis->sadd("user:$id:meetings" => $meeting);
-    $redis->quit;
-
-    my $hdr     = HTTP::Headers->new(Content_Type => 'application/json',
-                                     Connection   => 'close');
-    my $content = "";
-
-    my $resp = HTTP::Response->new(RC_OK, "", $hdr, $content);
-    $conn->send_response($resp);
-}
-
 sub createUser {
     my $conn = shift;
     my $req = shift;
@@ -298,9 +304,10 @@ sub createUser {
     debug('Look-up user <' . $id . "> with name <" . $nickname . ">" );
 
     # Check the user id is valid
-    if ( $id !~ /$unamePattern/ ) {
-        debug('Invalid user id');
-        $conn->send_error(400, 'Invalid user id');
+    my $valid = userValid($id);
+    if ( $valid == 0 ) {
+        debug('Invalid user id <' . $id . '>');
+        $conn->send_error(500, "Internal Error");
         return;
     }
 
@@ -345,22 +352,103 @@ sub createUser {
     $db->disconnect();
 }
 
-sub returnUser {
-    my $conn = shift;
-    my $id = shift;
-    debug('Look-up user <' . $id . '>');
+sub createMeeting {
+    my $conn  = shift;
+    my $req   = shift;
 
-    if( userExists( $id ) ) {
-        my $content = "{\"username\":\"" . $id . "\", \"name\":\"" . getNickname($id) . "\"}";
+    my $q = CGI->new($req->content);
+    my $id = $q->param('creator');
+    my $meetingName = $q->param('name');
+    debug('Asked to make meeting for <' . $id . '> called <' . $meetingName . '>');
 
-        my $hdr     = HTTP::Headers->new( Content_Type => 'application/json',
-                                          Connection   => 'close');
-        my $resp = HTTP::Response->new(RC_OK, "", $hdr, $content);
-        $conn->send_response($resp);
-    } else {
-        debug('User id not found');
-        $conn->send_error(404, "Participant not found");
+    my $valid = userValid($id);
+    if ( $valid == 0 ) {
+        debug('Invalid user id <' . $id . '>');
+        $conn->send_error(500, "Internal Error");
+        return;
     }
+
+    my $exists = userExists($id);
+    if ( $exists == 0 ) {
+        debug('User id <' . $id . '> not found');
+        $conn->send_error(404, "User id not found");
+        return;
+    } elsif ( $exists == -1 ) {
+        $conn->send_error(500, "Internal Error");
+        return;
+    }
+
+    my $redis;
+    unless ( $redis = Redis->new( server => 'localhost:6379', encoding => undef ) ) {
+        debug('Failed to connect to Redis');
+        $conn->send_error(500, 'Internal error');
+        return;
+    }
+
+    # Create the Redis keys for the meeting
+    my $meeting = $redis->incr('util:counter:meetings');
+    $redis->set("meeting:$meeting:name" => $meetingName);
+    $redis->set("meeting:$meeting:state" => 0);
+    $redis->sadd("meeting:$meeting:participants" => $id);
+    $redis->set("meeting:$meeting:$id:state" => 0);
+    $redis->set("meeting:$meeting:$id:notified" => 1);
+    $redis->sadd("util:list:meetings" => $meeting);
+    $redis->sadd("user:$id:meetings" => $meeting);
+    $redis->quit;
+
+    my $hdr     = HTTP::Headers->new(Content_Type => 'application/json',
+                                     Connection   => 'close');
+    my $content = "{\"id\":\"$meeting\"}";
+
+    my $resp = HTTP::Response->new(RC_OK, "", $hdr, $content);
+    $conn->send_response($resp);
+}
+
+sub addParticipant {
+    my $conn    = shift;
+    my $req     = shift;
+    my $meeting = shift;
+    
+    my $q = CGI->new($req->content);
+    my $id = $q->param('id');
+    debug('Add user <' . $id . "> to meeting <" . $meeting . ">" );
+
+    # Check the user id is valid
+    my $valid = userValid($id);
+    if ( $valid == 0 ) {
+        debug('Invalid user id <' . $id . '>');
+        $conn->send_error(500, "Internal Error");
+        return;
+    }
+
+    my $exists = userExists($id);
+    if ( $exists == 0 ) {
+        debug('User id <' . $id . '> not found');
+        $conn->send_error(404, "User id not found");
+        return;
+    } elsif ( $exists == -1 ) {
+        $conn->send_error(500, "Internal error");
+        return;
+    }
+
+    my $redis;
+    unless ( $redis = Redis->new( server => 'localhost:6379', encoding => undef ) ) {
+        debug('Failed to connect to Redis');
+        $conn->send_error(500, "Internal error");
+        return;
+    }
+    $redis->sadd("meeting:$meeting:participants" => $id);
+    $redis->set("meeting:$meeting:$id:state" => 0);
+    $redis->set("meeting:$meeting:$id:notified" => 0);
+    $redis->sadd("user:$id:meetings" => $meeting);
+    $redis->quit;
+
+    my $hdr     = HTTP::Headers->new(Content_Type => 'application/json',
+                                     Connection   => 'close');
+    my $content = "";
+
+    my $resp = HTTP::Response->new(RC_OK, "", $hdr, $content);
+    $conn->send_response($resp);
 }
 
 sub setParticipantStatus {
@@ -372,7 +460,25 @@ sub setParticipantStatus {
     my $q = CGI->new($req->content);
     my $state = $q->param('status');
     debug( 'Setting participant <' . $id . '> in meeting <' . $meeting . '> to state <' . $state . '>' );
-    
+
+    # Check the user id is valid
+    my $valid = userValid($id);
+    if ( $valid == 0 ) {
+        debug('Invalid user id <' . $id . '>');
+        $conn->send_error(500, "Internal Error");
+        return;
+    }
+
+    my $exists = userExists($id);
+    if ( $exists == 0 ) {
+        debug('User id <' . $id . '> not found');
+        $conn->send_error(404, "User id not found");
+        return;
+    } elsif ( $exists == -1 ) {
+        $conn->send_error(500, "Internal Error");
+        return;
+    }
+
     my $redis;
     if ( $redis = Redis->new( server => 'localhost:6379', encoding => undef ) ) {
         if ( $state eq 'ready' ) {
@@ -394,6 +500,18 @@ sub setParticipantStatus {
     $conn->send_response($resp);
 }
 
+# Check if a user is valid
+# Return 1 for valid
+#        0 for invalid
+sub userValid {
+    my $id = shift;
+
+    if ( $id =~ /$unamePattern/ ) {
+        return 1;
+    }
+    return 0;
+}
+
 # Check if a user exists.
 # Return 1 for exists
 #        0 for doesn't exist
@@ -406,6 +524,7 @@ sub userExists {
     if ( $redis = Redis->new( server => 'localhost:6379', encoding => undef ) ) {
         if ( $redis->get( "user:$id" ) ) {
             debug( 'Redis found user <' . $id . '>' );
+            $redis->expire( "user:$id", 100000);
             return 1;
         }
     } else {
@@ -447,6 +566,7 @@ sub getNickname {
         my $nickname = $redis->get("user:$id:nickname");
         if ( $nickname ) {
             debug( 'Redis found user <' . $id . '> with nickname <' . $nickname . '>' );
+            $redis->expire( "user:$id:nickname", 100000 );
             return $nickname;
         }
     } else {
