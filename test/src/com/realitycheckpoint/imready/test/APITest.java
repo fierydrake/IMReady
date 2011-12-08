3cb2162c8067477e8f48d622f949dbac82bdbb76
mjt <Mike@fierydrake.(none)>
2011-12-08 22:25:46 +0000

    Merge initial changes from other machine

--- test/src/com/realitycheckpoint/imready/test/APITest.java
+++ test/src/com/realitycheckpoint/imready/test/APITest.java
@@ -1,55 +1,81 @@
 package com.realitycheckpoint.imready.test;
 
+import java.util.UUID;
+
 import com.realitycheckpoint.imready.CreateMeeting;
 import com.realitycheckpoint.imready.client.API;
 import com.realitycheckpoint.imready.client.APICallFailedException;
 
 public class APITest extends android.test.ActivityInstrumentationTestCase2<CreateMeeting> {
-	private static final String TEST_SERVER_URI = "http://192.168.2.6:54321/";
+	private String primaryUserId;
+	private String secondaryUserId;
+
 	public APITest() {
 		super("com.realitycheckpoint.imready", CreateMeeting.class);
-		API.setServerURI(TEST_SERVER_URI);
 	}
 
 	public void setUp() throws Exception {
+		String suffix = UUID.randomUUID().toString().replaceAll("-", "_");
+		primaryUserId = "testuserA_" + suffix;
+		secondaryUserId = "testuserB_" + suffix;
 	}
 	
-	public void tearDown() throws Exception {
-	}
-	
-//	public void testMeetings() {
-//		try {
-//			assertNotNull("Listing meetings should not return null", API.meetings()); // TODO improve
-//		} catch (APICallFailedException e) {
-//			fail("API call failed: " + e);
-//		}
-//	}
-
-//	public void testMeetingParticipants() {
-//		try {
-//			int meetingId = API.createMeeting("mjt", "My Test Meeting");
-//			assertNotNull("Listing meeting participants should not return null", API.meetingParticipants(meetingId)); // TODO improve
-//		} catch (APICallFailedException e) {
-//			fail("API call failed: " + e);
-//		}
-//	}
-
-	public void testCreateUser() {
+	public void testUserResource() {
+		/* 
+		 * Create a new user
+		 */
 		try {
-			API.createUser("testuser", "Mr Test"); // TODO improve (need to clear db)
+			API.createUser(primaryUserId, "Mr Test");
 		} catch (APICallFailedException e) {
-			fail("API call failed: " + e);
+			e.printStackTrace();
+			fail("Failed to create user: " + e);
+		}
+		
+		/*
+		 * Test that the user now exists
+		 */
+		try {
+			API.user(primaryUserId);
+		} catch (APICallFailedException e) {
+			e.printStackTrace();
+			fail("Failed to retrieve user: " + e);
+		}
+		
+		/*
+		 * Test that the user we didn't create fails
+		 */
+		try {
+			API.user(secondaryUserId);
+			fail("User '" + secondaryUserId + "' should not exist");
+		} catch (APICallFailedException e) {
+			assertTrue("User should not be found", e.getMessage().contains("not found"));
 		}
 	}
-
-	public void testCreateMeeting() {
+	
+	public void testMeetingResource() {
 		try {
-			int meetingId = API.createMeeting("mjt", "My Test Meeting");
+			API.createUser(primaryUserId, "Mr Test A");
+			API.createUser(secondaryUserId, "Mr Test B");
+		} catch (APICallFailedException e) {
+			e.printStackTrace();
+			fail("Failed to create user: " + e);
+		}
+		
+		try {
+			int meetingId = API.createMeeting(primaryUserId, "Test Meeting");
 			assertTrue("Creating a meeting should return a valid meeting id (meetingId=" + meetingId + ")", meetingId > 0);
+			API.meeting(meetingId);
+//			API.addMeetingParticipant(meetingId, secondaryUserId);
+//			API.meetingParticipants(meetingId);
+			/* -- Check the paticipant was added */
+//			API.userMeetings(secondaryUserId);
+			/* -- Check the user is in the meeting */
 		} catch (APICallFailedException e) {
-			fail("API call failed: " + e);
+			e.printStackTrace();
+			fail("Failed to create meeting: " + e);			
 		}
 	}
+	
 
 //	public void testAddMeetingParticipant() {
 //		try {
